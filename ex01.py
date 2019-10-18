a7c8c7025d36e0597af9190c1681a996e9f96d15
woo289 <rhddbgkqtlek@naver.com>
2019-10-18 12:18:47 +0900

    다단계

--- ex01.py
+++ ex01.py
@@ -11,5 +11,9 @@ s = {"name":"김슬지","kor":100,"eng":80,"math":90}
 mycol.insert_one(s)
 print(s)
 
+dadange = [{"name":"이재우"},{"name":"천석훈"},{"name":"김슬지"},{"name":"김해리"},{"name":"장은지"}]
+
+for s in dadange:
+    print(s)
 
 
--- ex03.py
+++ ex03.py
@@ -1,5 +1,9 @@
 import bitutil.stu
 
-r= bitutil.stu.getStudent('김지선')
-print(type(r))
-print(r)
+# r= bitutil.stu.getStudent('김지선')
+# print(type(r))
+# print(r)
+dadange = [{"name":"이재우"},{"name":"천석훈"},{"name":"김슬지"},{"name":"김해리"},{"name":"장은지"}]
+
+for s in dadange:
+    print(s)
